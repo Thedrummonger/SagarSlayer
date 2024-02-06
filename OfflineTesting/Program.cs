683733ba7cf6c3454a261b8832b8b634a0322ea3
thedrummonger <drummerxtmail@gmail.com>
2024-02-06 13:19:23 -0600

    Add some TTS example code

--- OfflineTesting/Program.cs
+++ OfflineTesting/Program.cs
@@ -9,36 +9,65 @@ using System.IO.Compression;
 using System.Text;
 using static DrathBot.DataStructure.ExtendedDiscordObjects;
 using static DrathBot.DataStructure.StaticBotPaths.Sagarism;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
 
-string OldSagarQuotesCacheFile = Path.Combine(Directories.SagarismData, "SagarQuoteCacheOLD.json");
-var QuoteCache = Utility.GetfromFile<Misc.DistinctList<SerializeableDiscordMessage>>(StaticBotPaths.Sagarism.Files.SagarQuotesCacheFile);
-var OldCache = Utility.GetfromFile<Misc.DistinctList<SerializeableDiscordMessage>>(OldSagarQuotesCacheFile);
-QuoteCache.ResetAll();
-
-foreach(var oldUsedQuote in OldCache.Used)
+internal class Program
 {
-    var UsedQUote = QuoteCache.Unused.First(x => x.Content == oldUsedQuote.Content);
-    QuoteCache.GetUnused(QuoteCache.Unused.IndexOf(UsedQUote));
-    Debug.WriteLine($"Using Old Quote {UsedQUote.Content}");
-}
-
-File.WriteAllText(StaticBotPaths.Sagarism.Files.SagarQuotesCacheFile, QuoteCache.ToFormattedJson());
-
-Console.ReadLine();
-
-byte[] CompressByte(byte[] bytes)
-{
-    using var memoryStream = new MemoryStream();
-    using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.SmallestSize))
+    private static void Main(string[] args)
     {
-        gzipStream.Write(bytes, 0, bytes.Length);
-    }
-    return memoryStream.ToArray();
-}
 
-byte[] Compress(string String)
-{
-    byte[] dataToCompress = Encoding.UTF8.GetBytes(String);
-    byte[] compressedData = CompressByte(dataToCompress);
-    return compressedData;
+        Console.ReadLine();
+    }
+
+    void TestTTS()
+    {
+        using (SpeechSynthesizer synth = new SpeechSynthesizer())
+        {
+            synth.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Child);
+
+            synth.Rate = -2;
+
+            Stream ms = new MemoryStream();
+            //reader.SetOutputToWaveStream(ms);
+
+            Console.WriteLine("Speaking");
+            synth.Speak("Hello this is an example expression from the computers TTS engine in C-Sharp");
+            Console.WriteLine("Done Speaking");
+        }
+    }
+
+    void SyncUsedWithOldQuoteCache()
+    {
+        string OldSagarQuotesCacheFile = Path.Combine(Directories.SagarismData, "SagarQuoteCacheOLD.json");
+        var QuoteCache = Utility.GetfromFile<Misc.DistinctList<SerializeableDiscordMessage>>(Files.SagarQuotesCacheFile);
+        var OldCache = Utility.GetfromFile<Misc.DistinctList<SerializeableDiscordMessage>>(OldSagarQuotesCacheFile);
+        QuoteCache.ResetAll();
+
+        foreach (var oldUsedQuote in OldCache.Used)
+        {
+            var UsedQUote = QuoteCache.Unused.First(x => x.Content == oldUsedQuote.Content);
+            QuoteCache.GetUnused(QuoteCache.Unused.IndexOf(UsedQUote));
+            Debug.WriteLine($"Using Old Quote {UsedQUote.Content}");
+        }
+
+        File.WriteAllText(Files.SagarQuotesCacheFile, QuoteCache.ToFormattedJson());
+    }
+
+    byte[] CompressByte(byte[] bytes)
+    {
+        using var memoryStream = new MemoryStream();
+        using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.SmallestSize))
+        {
+            gzipStream.Write(bytes, 0, bytes.Length);
+        }
+        return memoryStream.ToArray();
+    }
+
+    byte[] Compress(string String)
+    {
+        byte[] dataToCompress = Encoding.UTF8.GetBytes(String);
+        byte[] compressedData = CompressByte(dataToCompress);
+        return compressedData;
+    }
 }
